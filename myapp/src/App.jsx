3517bc93ddf3055264b602761bcaf08170b54c4d
Manish Baheshwar <116203463+manishbaheshwar@users.noreply.github.com>
2024-02-03 23:33:30 +0530

    first commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import HeroSection from './Component/Hero';
+import Navigation from './Component/Navigation';
+const App = () => {
+
+  return (
+ <div>
+  <Navigation/>
+  <HeroSection/>
+
+ </div>
+  )
+}
+
+export default App;
